7c5fab5762d11c1e4d5471378595a2c7862825eb
AbdulhaqKhan1 <abdulhaqk113@gmail.com>
2025-05-15 23:39:34 +0100

    Image fixing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
           Yo, I’m just a young dude chasing my dreams with crazy energy and a whole lotta heart. I started out just having fun online, now I’m turning that grind into something real. Whether I’m screaming on stream, making people laugh, or just being me — it’s all love. I move fast, I go hard, and I keep it real every step of the way. Ain’t no slowing down — this is just the beginning.
         </p>
         <div className="about-images">
-          <img src="../src/assets/IMG_657.webp" alt="Profile" className="about-image" />
+          <img src="../src/assets/IMG_843.jpeg" alt="Profile" className="about-image" />
         </div>
       </div>
     </section>
