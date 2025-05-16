9c394684cba7c93e146a45accdf4799f29393ac7
AbdulhaqKhan1 <abdulhaqk113@gmail.com>
2025-05-16 22:17:47 +0100

    Page size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,6 @@ const HeroSection = () => {
           <h1 className="hero-title">Welcome to My World</h1>
           <p className="hero-description">This is where passion meets purpose. I’ve come a long way, turning my energy and love for entertaining into something real. Thanks for being here — let’s grow, have fun, and keep chasing greatness together</p>
 
-            {/* <h1 className="pluto-boy">Pluto Boy</h1> */}
     <h2 className="young">I Show Speed</h2>
     <p className="jobs">Artist | Influencer | Creator</p>
 
