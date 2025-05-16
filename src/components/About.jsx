107fb0191f61320a51114fd3664a856048f807cc
AbdulhaqKhan1 <abdulhaqk113@gmail.com>
2025-05-16 21:59:57 +0100

    Image Bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import profileImage from '../assets/IMG_843.png';
 
 const AboutPage = () => {
   return (
@@ -6,10 +7,10 @@ const AboutPage = () => {
       <div className="about-content">
         <h2 className="about-title">About Me</h2>
         <p className="about-description">
-          Yo, I’m just a young dude chasing my dreams with crazy energy and a whole lotta heart. I started out just having fun online, now I’m turning that grind into something real. Whether I’m screaming on stream, making people laugh, or just being me — it’s all love. I move fast, I go hard, and I keep it real every step of the way. Ain’t no slowing down — this is just the beginning.
+          Yo, I’m just a young dude chasing my dreams with crazy energy and a whole lotta heart...
         </p>
         <div className="about-images">
-          <img src="IMG_843.png" alt="Profile" className="about-image" />
+          <img src={profileImage} alt="Profile" className="about-image" />
         </div>
       </div>
     </section>
